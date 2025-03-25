d1589f7dc22e1887785d3c9f92dd1338b59b2e34
Valentino1746 <valentinomatamoros2@gmail.com>
2025-03-24 21:50:09 -0600

    Add files via upload

---
+++ Practicas/ejercicioAntonio/js/practonio.js
@@ -0,0 +1,6 @@
+let numero = 5;
+numero = numero + 10;
+
+function imprimirNumero(){
+	console.log("El resultado es: " + numero);
+}
---
+++ Practicas/ejercicioBere/js/corregir.js
@@ -0,0 +1,6 @@
+ let numero1 = 10;
+ let numero2 = 5;
+ let bere = numero1 + numero2;
+ if (bere === 15) {
+ console.log("El resultado es: " + bere);
+}
